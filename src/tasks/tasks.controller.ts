2cbda15b9158dd81e1999f4cda5fc21681cc8cd4
Hamza Khan <khanhamza124@gmail.com>
2021-02-19 20:45:39 +0530

    connection with postgresql

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -12,43 +12,43 @@ import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 export class TasksController {
     constructor(private taskService: TasksService){}
 
-    @Get()
-    getTasks(@Query(ValidationPipe) filterDto: GetClassFilterDto): Task[] {
-        if (Object.keys(filterDto).length) {
-            return this.taskService.getTasksWithFilter(filterDto);
-        } else  {
-            return this.taskService.getAllTasks();
+    // @Get()
+    // getTasks(@Query(ValidationPipe) filterDto: GetClassFilterDto): Task[] {
+    //     if (Object.keys(filterDto).length) {
+    //         return this.taskService.getTasksWithFilter(filterDto);
+    //     } else  {
+    //         return this.taskService.getAllTasks();
 
-        }
-    }
+    //     }
+    // }
 
-    @Get('/:id')
-    getTaskById(@Param('id') id:string): Task {
-        return this.taskService.getTaskById(id)
+    // @Get('/:id')
+    // getTaskById(@Param('id') id:string): Task {
+    //     return this.taskService.getTaskById(id)
 
-    }
+    // }
 
-    @Delete('/:id')
-    deleteTask(@Param('id') id:string) {
-        this.taskService.deleteTask(id);
-    };
+    // @Delete('/:id')
+    // deleteTask(@Param('id') id:string) {
+    //     this.taskService.deleteTask(id);
+    // };
 
-    @Patch('/:id/status')
-    editTask(@Param('id') id:string, 
-    @Body('status', TaskStatusValidationPipe) status:TaskStatus) {
-        return this.taskService.updateTaskStatus(id,status);
-    };
-
-
-    @Post()
-    @UsePipes(ValidationPipe)
-    @ApiBody({type: Task})
-    @ApiCreatedResponse({
-        description: 'Task Creation'
-    })
-    createTask(@Body() createTaskDto: CreateTaskDto): Task {
-        return this.taskService.createTask(createTaskDto)
+    // @Patch('/:id/status')
+    // editTask(@Param('id') id:string, 
+    // @Body('status', TaskStatusValidationPipe) status:TaskStatus) {
+    //     return this.taskService.updateTaskStatus(id,status);
+    // };
 
 
-    }
+    // @Post()
+    // @UsePipes(ValidationPipe)
+    // @ApiBody({type: Task})
+    // @ApiCreatedResponse({
+    //     description: 'Task Creation'
+    // })
+    // createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    //     return this.taskService.createTask(createTaskDto)
+
+
+    // }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -6,63 +6,62 @@ import { GetClassFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
-    private tasks: Task[]  = [];
 
-    getAllTasks() {
-        return this.tasks;
-    };
+    // getAllTasks() {
+    //     return this.tasks;
+    // };
 
-    getTasksWithFilter(filterDto: GetClassFilterDto): Task[] {
-        const { status, search } = filterDto;
+    // getTasksWithFilter(filterDto: GetClassFilterDto): Task[] {
+    //     const { status, search } = filterDto;
 
-        let tasks = this.getAllTasks();
+    //     let tasks = this.getAllTasks();
 
-        if (status) {
-            tasks = tasks.filter(task => task.status === status);
-        }
+    //     if (status) {
+    //         tasks = tasks.filter(task => task.status === status);
+    //     }
 
-        if (search) {
-            tasks = tasks.filter(task => 
-            task.title.includes(search) || task.description.includes(search),
-            ); 
-        }
-        return tasks
-    };
+    //     if (search) {
+    //         tasks = tasks.filter(task => 
+    //         task.title.includes(search) || task.description.includes(search),
+    //         ); 
+    //     }
+    //     return tasks
+    // };
 
-    getTaskById(id:string):Task {
-        const found =  this.tasks.find(task => task.id === id);
+    // getTaskById(id:string):Task {
+    //     const found =  this.tasks.find(task => task.id === id);
 
-        if (!found) {
-            throw new NotFoundException(`Task with "${id}" not found`);
-        }
-        return found;
-    }
+    //     if (!found) {
+    //         throw new NotFoundException(`Task with "${id}" not found`);
+    //     }
+    //     return found;
+    // }
 
-    createTask(createTaskDto:CreateTaskDto): Task {
-        const { title, description} = createTaskDto;
+    // createTask(createTaskDto:CreateTaskDto): Task {
+    //     const { title, description} = createTaskDto;
         
-        const task: Task = {
-            id: uuidv4(),
-            title,
-            description,
-            status: TaskStatus.OPEN
-        };
-        this.tasks.push(task);
-        return task
-    };
+    //     const task: Task = {
+    //         id: uuidv4(),
+    //         title,
+    //         description,
+    //         status: TaskStatus.OPEN
+    //     };
+    //     this.tasks.push(task);
+    //     return task
+    // };
 
-    updateTaskStatus(id:string, status:TaskStatus): Task {
-        const task = this.getTaskById(id);
-        task.status = status;
-        return task 
+    // updateTaskStatus(id:string, status:TaskStatus): Task {
+    //     const task = this.getTaskById(id);
+    //     task.status = status;
+    //     return task 
         
-    };
+    // };
 
 
-    deleteTask(id:string):void {
-        const found = this.getTaskById(id);
-        this.tasks = this.tasks.filter(task => task.id !== found.id);
-    };
+    // deleteTask(id:string):void {
+    //     const found = this.getTaskById(id);
+    //     this.tasks = this.tasks.filter(task => task.id !== found.id);
+    // };
 
 
 }
