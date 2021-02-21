f976562dfd8f00b7b6df729d584c09de4e8cdca0
Hamza Khan <khanhamza124@gmail.com>
2021-02-21 19:39:05 +0530

    auth signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiBody, ApiCreatedResponse } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private authService: AuthService,
+    ) { }
+
+    @Post('/signup')
+    @ApiBody({ type: User })
+    @ApiCreatedResponse({
+        description: 'SignUp'
+    })
+    signup(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.authService.signup(authCredentialsDto);
+    }
+}
