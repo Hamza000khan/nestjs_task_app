3a37b54a20d3c5118a9e538f7597651e4e77ea18
Hamza Khan <khanhamza124@gmail.com>
2021-02-22 16:37:14 +0530

    Signing a JWT Token Upon Authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,36 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { userInfo } from 'os';
 import { resourceLimits } from 'worker_threads';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtPayload } from './jwt-payload.interface';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
-    private userRepository: UserRepository
+    private userRepository: UserRepository,
+    private jwtService: JwtService
   ) {}
 
   async signup(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto) {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto
+  ): Promise<{ accesToken: string }> {
     const username = await this.userRepository.validateUserPassword(
       authCredentialsDto
     );
     if (!username) {
       throw new UnauthorizedException('Invalid Credentials');
     }
+    const payload: JwtPayload = { username };
+    const accesToken = await this.jwtService.sign(payload);
+
+    return { accesToken };
   }
 }
