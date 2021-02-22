b92d3ef137e08e2c41b53d978df96a00d453a0c3
Hamza Khan <khanhamza124@gmail.com>
2021-02-22 11:43:44 +0530

    Feature Validating Password - Sign In

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,14 +6,21 @@ import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
-    constructor(
-        private authService: AuthService,
-    ) { }
+  constructor(private authService: AuthService) {}
 
-    @Post('/signup')
-    @ApiBody({ type: User })
-    @ApiCreatedResponse({ description: 'Min Length - 4      Max Length - 20' })
-    signup(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void> {
-        return this.authService.signup(authCredentialsDto);
-    }
+  @Post('/signup')
+  @ApiBody({ type: User })
+  @ApiCreatedResponse({ description: 'Min Length - 4      Max Length - 20' })
+  signup(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto
+  ): Promise<void> {
+    return this.authService.signup(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  @ApiBody({ type: User })
+  @ApiCreatedResponse({ description: 'User SignIn' })
+  signin(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+    return this.authService.signIn(authCredentialsDto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { userInfo } from 'os';
+import { resourceLimits } from 'worker_threads';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        @InjectRepository(UserRepository)
-        private userRepository: UserRepository,
-    ) { };
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository
+  ) {}
 
-    async signup(authCredentialsDto: AuthCredentialsDto): Promise<void> {
-        return this.userRepository.signUp(authCredentialsDto);
+  async signup(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.signUp(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto) {
+    const username = await this.userRepository.validateUserPassword(
+      authCredentialsDto
+    );
+    if (!username) {
+      throw new UnauthorizedException('Invalid Credentials');
     }
+  }
 }
