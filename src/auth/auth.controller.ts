5e0f15f93af8dc1b04e2d42b9e07f74217760668
Hamza khan <khanhamza124@gmail.com>
2021-05-23 14:15:39 +0530

    Changes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,32 +1,26 @@
-import {
-  Body,
-  Controller,
-  Post,
-  ValidationPipe,
-} from '@nestjs/common';
-import { ApiBearerAuth, ApiBody, ApiCreatedResponse } from '@nestjs/swagger';
-import { AuthService } from './auth.service';
-import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { User } from './user.entity';
+import { Body, Controller, Post, ValidationPipe } from "@nestjs/common";
+import { ApiBearerAuth, ApiBody, ApiCreatedResponse } from "@nestjs/swagger";
+import { AuthService } from "./auth.service";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { User } from "./user.entity";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('/signup')
+  @Post("/signup")
   @ApiBody({ type: User })
-  @ApiCreatedResponse({ description: 'Min Length - 4      Max Length - 20' })
+  @ApiCreatedResponse({ description: "Min Length - 4      Max Length - 20" })
   signup(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto
   ): Promise<void> {
     return this.authService.signup(authCredentialsDto);
   }
 
-  @Post('/signin')
+  @Post("/signin")
   @ApiBody({ type: User })
-  @ApiCreatedResponse({ description: 'User SignIn' })
+  @ApiCreatedResponse({ description: "User SignIn" })
   signin(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
     return this.authService.signIn(authCredentialsDto);
   }
-
 }
